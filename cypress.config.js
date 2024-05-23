66464742f6c1120edae0eced49777c7c1416318e
dday33480 <daviddufau@hotmail.co.uk>
2024-05-23 16:57:46 +0200

    Configures mochawesome reporting

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,18 @@ const { defineConfig } = require("cypress");
 const { downloadFile } = require("cypress-downloadfile/lib/addPlugin");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       on("task", { downloadFile });
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
